b4eee8e9bb936a137daaaa1222df8dbe18e16f2f
Miss-Louisa88 <167814167+Miss-Louisa88@users.noreply.github.com>
2024-05-13 00:34:17 +0300

    Added a Display Forecast function in JS
    This function aims at injecting javascript in html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("Louise is a rich developer");
+/// To correct this code
+function displayForecast(response) {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">🌧️</div>
+      <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+      <stong> 15°C </strong> </div>
+      <div class="weather-forecast-temperature"> 9°C </div> </div></div>
+
+    `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
