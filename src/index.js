9f07955eb944d892102d1e9023b11f071cef2dd5
Miss-Louisa88 <167814167+Miss-Louisa88@users.noreply.github.com>
2024-05-13 03:00:34 +0300

    Added a display Forecast function in JavaScript
    This function helps to get the API url for the forecast data of the cities.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
+function refreshWeather(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
 function searchCity(city) {
   let apiKey = "594o02104b870cb39413fdbd5t9a24d2";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=Paris&key=${apikey}&units=metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
   console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-/// To correct this code
+///Final function element
+function handleSearchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchFormInput");
+  searchCity(searchInput.value);
+}
+///This function displays the forecast information from api and injects it on html
 function displayForecast(response) {
   let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
-  forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      ` <div class="weather-forecast-day">
+  (forecastHtml = ""),
+    days.forEach(function (day) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
       <div class="weather-forecast-date">${day}</div>
-      <div class="weather-forecast-icon">🌧️</div>
+      <div class="weather-forecast-icon"></div>
       <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
-      <stong> 15°C </strong> </div>
-      <div class="weather-forecast-temperature"> 9°C </div> </div></div>
+      <strong> 15 </strong> </div>
+      <div class="weather-forecast-temperature"> 9</div>
+      </div> </div>`;
+    });
 
-    `;
-  });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
+
+let searchFormatElement = document.querySelector("#searchCity");
+searchFormatElement.addEventListener("submit", handleSearchCity);
